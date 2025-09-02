277c6bfacb6bca028ddaa6f2ba241e4d63be557c
GavaaJ <gavaaj@gmail.com>
2025-09-02 19:31:50 +1000

    update

--- terraform_demo/variables.tf
+++ terraform_demo/variables.tf
@@ -98,3 +98,8 @@ variable "ssh_private_key" {
   type        = string
   sensitive   = true # Marks as sensitive to hide in logs
 }
+
+variable "deploy_dags" {
+  description = "Flag to deploy DAGs to the Airflow instance"
+  type        = bool
+}
