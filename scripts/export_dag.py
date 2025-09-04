17f6ccd9cbf72b44cd213ffd92efca62c4c8e2df
GavaaJ <gavaaj@gmail.com>
2025-09-04 19:22:17 +1000

    update

--- scripts/export_dag.py
+++ scripts/export_dag.py
@@ -8,7 +8,7 @@ import pyarrow.parquet as pq
 from airflow.decorators import dag, task
 from airflow.models import Variable
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.decorators import get_current_context
+from airflow.operators.python import get_current_context
 
 # --- Config (edit or set as Airflow Variables) ---
 POSTGRES_CONN_ID = Variable.get("PG_CONN_ID", default_var="postgres_default")
@@ -34,6 +34,11 @@ def _export_one_table(table: str, ds: str):
                 continue
 
             if table == "customers":
+                df['id_number'] = df['id_number'].astype(str)
+                df['phone_number'] = df['phone_number'].astype(str)
+                df['postal_code'] = df['postal_code'].astype(str)
+                df['credit_score'] = df['credit_score'].astype(str)
+
                 schema_pa = pa.schema([
                     pa.field('customer_code', pa.string()),
                     pa.field('first_name', pa.string()),
